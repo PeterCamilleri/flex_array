417072c307938fc930840f7a7202bf33d3e37c64
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-04 12:25:12 -0400

    Clean up flex_array_process.rb

--- lib/flex_array/flex_array_process.rb
+++ lib/flex_array/flex_array_process.rb
@@ -1,18 +1,9 @@
-#* flex_array_process.rb - The flexible array class index processing routines.
+# The flexible array class index processing routines.
 class FlexArray
   private
-  #Process a \FlexArray index array. This is the heart of the \FlexArray
-  #indexing process.
-  #<br>Parameters
-  #* indexes - An array with as many entries as the flexible array has
-  #  dimensions. See [] for more details. Note that since indexes is NOT
-  #  a splat parameter, it must be passed as an array explicitly. If passed
-  #  in as [:all] all elements of the array are processed.
-  #* block - a block to be called for each value selected by this process.
-  #<br>Block Arguments
-  #* current - An array with one element per dimension with the fully qualified
-  #  index of the element being accessed.
-  #* posn - The position of the element being accessed in the data array.
+
+  # Process a \FlexArray index array. This is the heart of the flex array
+  # indexing process.
   def process_indexes(indexes, &block)
     current = Array.new(dimensions, 0)
 
@@ -29,33 +20,18 @@ class FlexArray
     end
   end
 
-  #The worker bee for process_indexes.
-  #<br>Parameters
-  #* depth - The index of the dimension being processed.
-  #* posn - The partially computed position of the element being accessed
-  #  in the data array.
-  #* indexes - An array of array indexes. See the method [] for details on
-  #  what sorts of things these can be.
-  #* current - The partial fully qualified index of the element being accessed.
-  #* block - A block to be called for each value selected by this process.
-  #<br>Block Arguments
-  #* current - An array with one element per dimension with the fully qualified
-  #  index of the element being accessed.
-  #* posn - The position of the element being accessed in the data array.
-  #<br>Note
-  #This is a recursive function. The recursion runs for index in 0..#dimensions
-  #<br>Endemic Code Smells
-  # :reek:LongParameterList
+  # The worker bee for process_indexes.
   def process_indexes_worker(depth, posn, indexes, current, &block)
-    if depth == dimensions                 #Is there more work to do?
-      block.call(current, posn)            #Index ready, call the block.
+    if depth == dimensions                 # Is there more work to do?
+      block.call(current, posn)            # Index ready, call the block.
     else
-      spec = @array_specs[depth]           #Get the current specification.
-      min, stride = spec.min, spec.stride  #Extract the relevant info.
+      spec = @array_specs[depth]           # Get the current specification.
+      min, stride = spec.min, spec.stride  # Extract the relevant info.
 
-      indexes[depth].each do |index|       #Iterate over the range.
-        current[depth] = index             #Update the current index.
-        #Process the next component in the array index.
+      indexes[depth].each do |index|       # Iterate over the range.
+        current[depth] = index             # Update the current index.
+
+        # Process the next component in the array index.
         process_indexes_worker(depth+1,
                                posn + (index-min) * stride,
                                indexes,
@@ -65,43 +41,26 @@ class FlexArray
     end
   end
 
-  #Special case where all of the array is being processed.
-  #<br>Parameters
-  #* block - A block to be called for each value selected by this process.
-  #<br>Block Arguments
-  #* current - An array with one element per dimension with the fully qualified
-  #  index of the element being accessed.
-  #* posn - The position of the element being accessed in the data array.
+  # Special case where all of the array is being processed.
   def process_all(&block)
     current = Array.new(dimensions, 0)
     process_all_worker(0, 0, current, &block)
   end
 
-  #The worker bee for process_all.
-  #<br>Parameters
-  #* depth - The index of the dimension being processed.
-  #* posn - The partially computed position of the element being accessed
-  #  in the data array.
-  #* current - The partial fully qualified index of the element being accessed.
-  #* block - A block to be called for each value selected by this process.
-  #<br>Block Arguments
-  #* current - An array with one element per dimension with the fully qualified
-  #  index of the element being accessed.
-  #* posn - The position of the element being accessed in the data array.
-  #<br>Note
-  #This is a recursive function. The recursion runs for index in 0..#dimensions
+  # The worker bee for process_all.
   def process_all_worker(depth, posn, current, &block)
-    if depth == dimensions                 #Is there more work to do?
-      block.call(current, posn)            #Index ready, call the block.
+    if depth == dimensions                 # Is there more work to do?
+      block.call(current, posn)            # Index ready, call the block.
     else
-      spec = @array_specs[depth]           #Get the current specification.
+      spec = @array_specs[depth]           # Get the current specification.
       stride = spec.stride
 
-      spec.each do |index|                 #Iterate over the range.
-        current[depth] = index             #Update the current index.
-        #Process the next component in the array specification.
+      spec.each do |index|                 # Iterate over the range.
+        current[depth] = index             # Update the current index.
+
+        # Process the next component in the array specification.
         process_all_worker(depth+1, posn, current, &block)
-        posn += stride                     #Step to the next position.
+        posn += stride                     # Step to the next position.
       end
     end
   end
