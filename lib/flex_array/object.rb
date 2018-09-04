8ebf488ae244956ad3e81a95b191e0887257d9dd
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-04 15:49:19 -0400

    Clean up object.rb

--- lib/flex_array/object.rb
+++ lib/flex_array/object.rb
@@ -1,21 +1,12 @@
-#Extensions to Object needed to support flex array.
+# Extensions to Object needed to support flex array.
+
 class Object
-  #Fail with message since the array dimension is invalid.
-  #<br>Parameters
-  #* _stride - unused, required to maintain signature.
-  #<br>Returns
-  #* Nothing, always fails.
+  # Fail with message since the array dimension is invalid.
   def to_spec_component(_stride)
     fail ArgumentError, "Invalid flex array dimension: #{self.inspect}"
   end
 
-  #Convert this object to an range index against the spec.
-  #<br>Parameters
-  #* spec - The spec component used to validate this index.
-  #<br>Returns
-  #* A range.
-  #<br>Exceptions
-  #* IndexError if the range is not valid.
+  # Convert this object to an range index against the spec.
   def to_index_range(spec)
     if self == :all
       spec.range
