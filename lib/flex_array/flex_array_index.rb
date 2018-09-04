0da9a26e2811ea49f4f6401734dd0a6be31026dd
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-04 12:17:13 -0400

    Clean up flex_array_index.rb

--- lib/flex_array/flex_array_index.rb
+++ lib/flex_array/flex_array_index.rb
@@ -1,19 +1,7 @@
-#* flex_array_index.rb - The flexible array indexing and related methods.
+# The flexible array indexing and related methods.
+
 class FlexArray
-  #Retrieve the selected data from the \FlexArray.
-  #<br>Parameters
-  #* indexes - An array with as many entries as the flexible array has
-  #  dimensions. These entries may be an integer that is in the bounds of
-  #  the range limit of that dimension, or it can be a range where the start
-  #  and end of the range are in the bounds of the range limit of that
-  #  dimension, or it can be the symbol :all for all of the possible values
-  #  of that dimension.
-  #<br>Returns
-  #* The data selected by the index or an array of data if the index selects
-  #  more than one cell.
-  #<br>Note
-  #* If the indexes parameter equals a single :all this maps to all
-  #  elements of the array, regardless of its dimensionality.
+  # Retrieve the selected data from the flex array.
   def [](*indexes)
     validate_index_count(indexes)
     result = []
@@ -21,20 +9,7 @@ class FlexArray
     result.length == 1 ? result[0] : result
   end
 
-  #Store the value data into the \FlexArray.
-  #<br>Parameters
-  #* indexes - An array with as many entries as the flexible array has
-  #  dimensions. These entries may be an integer that is in the bounds of
-  #  the range limit of that dimension, or it can be a range where the start
-  #  and end of the range are in the bounds of the range limit of that
-  #  dimension, or it can be the symbol :all for all of the possible values
-  #  of that dimension.
-  #* value - A value to be stored in the selected array entries.
-  #<br>Returns
-  #* The value stored in the array.
-  #<br>Note
-  #* If the indexes parameter equals a single :all this maps to all
-  #  elements of the array, regardless of its dimensionality.
+  # Store the value data into the flex array.
   def []=(*indexes, value)
     validate_index_count(indexes)
     source = value.in_array.cycle
