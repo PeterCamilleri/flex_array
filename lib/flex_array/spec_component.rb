6182beb8174c0cb2db45c7d55da868cf62eec2c7
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-04 15:55:36 -0400

    Clean up spec_component.rb

--- lib/flex_array/spec_component.rb
+++ lib/flex_array/spec_component.rb
@@ -1,50 +1,44 @@
-#This helper class encapsulates a single component of the flex array spec.
+# This helper class encapsulates a single component of the flex array spec.
+
 class SpecComponent
-  #The range of acceptable values for this limit.
+  # The range of acceptable values for this limit.
   attr_reader :range
 
-  #The stride of this array dimension. That is, for each step in this
-  #dimension, how many steps are required in the low level array?
+  # The stride of this array dimension. That is, for each step in this
+  # dimension, how many steps are required in the low level array?
   attr_reader :stride
 
-  #Create a limits component from its constituent data.
-  #<br>Parameters
-  #* range - the range of values for this index limit.
-  #* stride - the number of cells separating data with adjacent indexes.
+  # Create a limits component from its constituent data.
   def initialize(range, stride)
     @range, @stride = range, stride
   end
 
-  #Forward '=== value' to the range.
+  # Forward '=== value' to the range.
   def ===(value)
     @range === value
   end
 
-  #Limits are equal if their ranges are equal.
-  #<br>Returns
-  #* true if the spec components have equal ranges.
+  # Limits are equal if their ranges are equal.
   def ==(other)
     @range == other.range
   end
 
-  #Forward 'min' to the range.
+  # Forward 'min' to the range.
   def min
     @range.min
   end
 
-  #Forward 'max' to the range.
+  # Forward 'max' to the range.
   def max
     @range.max
   end
 
-  #Forward 'each' and the block to the range.
+  # Forward 'each' and the block to the range.
   def each(&block)
     @range.each(&block)
   end
 
-  #Compute the span of indexes in this limit component.
-  #<br>Returns
-  #* The span of the range or zero if there is none.
+  # Compute the span of indexes in this limit component.
   def span
     if @range.none?
       0
@@ -53,7 +47,7 @@ class SpecComponent
     end
   end
 
-  #Enlarge the range of this spec by the growth term.
+  # Enlarge the range of this spec by the growth term.
   def enlarge(growth)
     if @range.none?
       @range = 0...growth
@@ -62,9 +56,7 @@ class SpecComponent
     end
   end
 
-  #Compute the step required for the index value.
-  #<br>Returns
-  #* The number of array cells to be skipped for this index.
+  # Compute the step required for the index value.
   def index_step(index)
     (index - @range.min) * @stride
   end
