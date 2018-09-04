5172bbe6174eb00cf23f0be503793b93ef0c159f
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-04 15:53:28 -0400

    Clean up spec_array.rb

--- lib/flex_array/spec_array.rb
+++ lib/flex_array/spec_array.rb
@@ -1,4 +1,5 @@
-#This helper class encapsulates an array of flex array spec components.
+# This helper class encapsulates an array of flex array spec components.
+
 class SpecArray < Array
   #The number of elements defined by this array specification.
   attr_reader :spec_count
@@ -6,12 +7,7 @@ class SpecArray < Array
   #The number of dimensions specified by this specification
   alias spec_dimensions length
 
-  #Create a flex array specification.
-  #<br>Parameters
-  #* array_specs - The specification of the flex array subscript limits.
-  #  These can either be an array of containing integers, in which case
-  #  the limits are 0...n or they can be ranges, in which case the
-  #  limits are those of the range.
+  # Create a flex array specification.
   def initialize(array_specs)
     super(0)
     @spec_count = 1
@@ -25,7 +21,7 @@ class SpecArray < Array
     self
   end
 
-  #Is this array specification transposed in any way?
+  # Is this array specification transposed in any way?
   def transposed?
     check = 1
 
@@ -37,7 +33,7 @@ class SpecArray < Array
     false
   end
 
-  #Enlarge the flex array along its first dimension.
+  # Enlarge the flex array along its first dimension.
   def enlarge(growth)
     self[0].enlarge(growth)
 
