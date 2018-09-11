9bf9d6c98abe2e1bd01e78e5fbd446b4fd7aaed5
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-11 13:18:01 -0400

    Refined benchmarks.

--- bench/benchmark.rb
+++ bench/benchmark.rb
@@ -9,7 +9,11 @@ puts
 
 sz = 10000
 s2 = 100    # s2 * s2 == sz
+s3 = 10     # s3 * s3 * 2s == sz
 ct = 1000
+
+puts "Creating arrays:"
+puts "=============================================================================="
 
 Benchmark.bmbm do |x|
 
@@ -17,24 +21,41 @@ Benchmark.bmbm do |x|
     ct.times { Array.new(sz, "cat") }
   }
 
+  x.report("Flex 1d obj:") {
+    ct.times { FlexArray.new(sz, "cat") }
+  }
+
+  x.report("Flex 2d obj:") {
+    ct.times { FlexArray.new([s2,s2], "cat") }
+  }
+
+  x.report("Flex 3d obj:") {
+    ct.times { FlexArray.new([s3,s3,s2], "cat") }
+  }
+
+  x.report("Flex 4d obj:") {
+    ct.times { FlexArray.new([s3,s3,s3,s3], "cat") }
+  }
+
+
   x.report("Array blk:") {
     ct.times { Array.new(sz) {"cat"} }
   }
 
-  x.report("Flex obj:") {
-    ct.times { FlexArray.new(sz, "cat") }
-  }
-
-  x.report("Flex blk:") {
+  x.report("Flex 1d blk:") {
     ct.times { FlexArray.new(sz) {"cat"} }
   }
 
-  x.report("Flex2 obj:") {
-    ct.times { FlexArray.new([s2,s2], "cat") }
+  x.report("Flex 2d blk:") {
+    ct.times { FlexArray.new([s2,s2]) {"cat"} }
   }
 
-  x.report("Flex2 blk:") {
-    ct.times { FlexArray.new([s2,s2]) {"cat"} }
+  x.report("Flex 3d blk:") {
+    ct.times { FlexArray.new([s3,s3,s2]) {"cat"} }
+  }
+
+  x.report("Flex 4d blk:") {
+    ct.times { FlexArray.new([s3,s3,s3,s3]) {"cat"} }
   }
 
 end
