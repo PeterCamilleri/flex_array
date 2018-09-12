d7301b41d0fc7e12120ea69eee208ec8c50e62fe
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-12 13:45:38 -0400

    Added some more benchmarks.

--- bench/benchmark.rb
+++ bench/benchmark.rb
@@ -10,13 +10,23 @@ puts
 enable_creating_tests   = false
 enable_accessing_tests  = true
 
-ct  = 1000
+ct     = 1000
 
 sz  = 10000
 s2d = [100, 100]
 s3d = [10, 10, 100]
 s4d = [10, 10, 10, 10]
 s5d = [5, 5, 4, 10, 10]
+
+c1s = [1..9998]
+c2s = [1..98, 1..98]
+c3s = [1..8, 1..8, 1..98]
+c4s = [1..8, 1..8, 1..8, 1..8]
+c5s = [1..3, 1..3, 1..2, 1..8, 1..8]
+
+
+cyc_ct = ct*sz
+
 
 puts "Creating arrays:"
 puts "=============================================================================="
@@ -108,14 +118,14 @@ puts "Accessing arrays:"
 puts "=============================================================================="
 
 if enable_accessing_tests
-  array = Array.new(sz, 9)
-  fa_1d = FlexArray.new(sz, 9)
-  fa_2d = FlexArray.new(s2d, 9)
-  fa_3d = FlexArray.new(s3d, 9)
-  fa_4d = FlexArray.new(s4d, 9)
-  fa_5d = FlexArray.new(s5d, 9)
+  array =     Array.new(sz,  1.0)
+  fa_1d = FlexArray.new(sz,  1.0)
+  fa_2d = FlexArray.new(s2d, 1.0)
+  fa_3d = FlexArray.new(s3d, 1.0)
+  fa_4d = FlexArray.new(s4d, 1.0)
+  fa_5d = FlexArray.new(s5d, 1.0)
 
-  temp = 0
+  temp = 0.0
 
   Benchmark.bmbm do |x|
 
@@ -141,6 +151,63 @@ if enable_accessing_tests
 
     x.report("Flex 5d each:") {
       ct.times { fa_5d.each { |item| temp += item } }
+    }
+
+
+    x.report("Array cycle:") {
+      cyc = array.cycle
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 1d cycle:") {
+      cyc = fa_1d.cycle
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 2d cycle:") {
+      cyc = fa_2d.cycle
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 3d cycle:") {
+      cyc = fa_3d.cycle
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 4d cycle:") {
+      cyc = fa_4d.cycle
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 5d cycle:") {
+      cyc = fa_5d.cycle
+      cyc_ct.times { temp += cyc.next }
+    }
+
+
+    x.report("Flex 1d cysel:") {
+      cyc = fa_1d.select_cycle(c1s)
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 2d cysel:") {
+      cyc = fa_2d.select_cycle(c2s)
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 3d cysel:") {
+      cyc = fa_3d.select_cycle(c3s)
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 4d cysel:") {
+      cyc = fa_4d.select_cycle(c4s)
+      cyc_ct.times { temp += cyc.next }
+    }
+
+    x.report("Flex 5d cysel:") {
+      cyc = fa_5d.select_cycle(c5s)
+      cyc_ct.times { temp += cyc.next }
     }
 
   end
